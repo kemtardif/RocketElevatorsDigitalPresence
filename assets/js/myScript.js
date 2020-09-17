80cf25f3cb0d9bd47ac5019b0f380089637f3155
Kem Tardif <tardif.kremlin@gmail.com>
2020-09-16 23:23:22 -0400

    Final for real

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -59,292 +59,297 @@ $("#selecthyb").change( function() {
 
  //COMMERCIAL CALCULATIONS //////////////////////////////
 
- function calculateCom() {
-    var choiceCom = $("#selectcom").val();
-    var total = $("#deployed").val();
+ $("#buttoncom").click(function(event) {
 
-    $("#deploycom").html(total.toString());
-
-    if (choiceCom == "1") {
-
-        $("#unitcom").html("7,565.00");
-
-        x3 = 0.1*7565*total;
-        x3 = x3.formatMoney(2, '.', ',');
-        $("#feescom").html(x3);
-
+    event.preventDefault();
     
-        total= (1+0.1)*7565*total;
-        y3 = total.formatMoney(2, '.', ',');
-        $("#totalcom").html(y3);
+    var variableCom = {
 
-    } else if (choiceCom == "2") {
+        choiceCom : $("#selectcom").val(),
+        total : $("#deployed").val()
+    };
+    
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(variableCom),
+        contentType: 'application/json',
+        url: 'https://pacific-forest-47162.herokuapp.com/comserv',						
+        success: function(data) {
+            console.log(data); 
+    
+        total = data.tot;
+        select = data.sel;
 
-        $("#unitcom").html("12,345.00");
+        $("#deploycom").html(total.toString());
+
+        if (select == "1") {
         
-        x3 = 0.13*12345*total;
-        x3 = x3.formatMoney(2, '.', ',');
-        $("#feescom").html(x3);
-
+            $("#unitcom").html("7,345.00");
     
-        total= (1+0.13)*12345*total;
-        y3 = total.formatMoney(2, '.', ',');
-        $("#totalcom").html(y3);
-
-    } else if (choiceCom == "3") {
-
-        $("#unitcom").html("15,400.00");
-        
-        x3 = 0.16*15400*total;
-        x3 = x3.formatMoney(2, '.', ',');
-        $("#feescom").html(x3);
-
+            y = 0.1*7565*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feescom").html(y);
     
-        total= (1+0.16)*15400*total;
-        y3 = total.formatMoney(2, '.', ',');
-        $("#totalcom").html(y3);
-    }
- }
+            totalHyb= (1+0.1)*7565*total;
+            x = totalHyb.formatMoney(2, '.', ',')
+            $("#totalcom").html(x);
+    
+    
+        } else if (select == "2") {
+    
+            $("#unitcom").html("12,345.00");
+    
+            y = 0.13*12345*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feescom").html(y);
+           
+    
+            totalHyb = (1+0.13)*12345*total;
+            x = totalHyb.formatMoney(2, '.', ',');
+            $("#totalcom").html(x);
+    
+           
+    
+        } else if (select == "3") {
+    
+            $("#unitcom").html("15,400.00");
+    
+           y = 0.16*15400*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feescom").html(y);
+    
+            totalHyb= (1+0.16)*15400*total;
+            x = totalHyb.formatMoney(2, '.', ',');
+            $("#totalcom").html(x);
+        }}
+    });
+});
 
-$("#selectcom").change(calculateCom);
-$("#deployed").change(calculateCom);
 
 
 //CORPORATE CALCULATIONS ////////////////////////////
 
+$("#buttoncorp").click(function(event) {
 
-
-
-function calculateCorp() {
-    var selectCorp = $("#selectcorp").val();
-    var tenantsCorp = $("#tenants").val();
-    var basementsCorp = $("#bascorp").val();
-    var floorsCorp = $("#floorcorp").val();
-
-    f = Number(floorsCorp);
-    b = Number(basementsCorp);
-
-    stories = f + b;
-
-    totalTenants = stories*tenantsCorp;
-
-    elevatorsRequired = Math.ceil(totalTenants/1000);
-
-    columnsRequired = Math.ceil(stories/20);
-
-    elevatorPerColumn = Math.ceil(elevatorsRequired/columnsRequired);
-
-    totalEls = elevatorPerColumn*columnsRequired;
-
-    $("#deploycorp").html(totalEls);
-
-   
-
-    if (selectCorp == "1") {
-
-        $("#unitcorp").html("7,565.00");
-
-        x2 = 0.1*7565*totalEls;
-        x2 = x2.formatMoney(2, '.', ',');
-        $("#feescorp").html(x2);
-
+    event.preventDefault();
     
-        totalCorp= (1+0.1)*7565*totalEls;
-        y2 = totalCorp.formatMoney(2, '.', ',');
-        $("#totalcorp").html(y2);
-        
-    } else if (selectCorp == "2") {
+    var variableCorp = {
 
-        $("#unitcorp").html("12,345.00");
-        
-        x2 = 0.13*12345*totalEls;
-        x2 = x2.formatMoney(2, '.', ',');
-        $("#feescorp").html(x2);
+    selectCorp : $("#selectcorp").val(),
+    tenantsCorp : $("#tenants").val(),
+    basementsCorp : $("#bascorp").val(),
+    floorsCorp : $("#floorcorp").val()
 
+    };
     
-        totalCorp= (1+0.13)*12345*totalEls;
-        y2 = totalCorp.formatMoney(2, '.', ',');
-        $("#totalcorp").html(y2);
-
-       
-
-    } else if (selectCorp == "3") {
-
-        $("#unitcorp").html("15,400.00");
-        
-        x2 = 0.16*15400*totalEls;
-        x2 = x2.formatMoney(2, '.', ',');
-        $("#feescorp").html(x2);
-
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(variableCorp),
+        contentType: 'application/json',
+        url: 'https://pacific-forest-47162.herokuapp.com/corpserv',						
+        success: function(data) {
+            console.log(data); 
     
-        totalCorp= (1+0.16)*15400*totalEls;
-        y2 = totalCorp.formatMoney(2, '.', ',');
-        $("#totalcorp").html(y2);
-       
-    }
- }
+        total = data.tot;
+        select = data.sel;
 
-$("#selectcorp").change(calculateCorp);
-$("#tenants").change(calculateCorp);
-$("#floorcorp").change(calculateCorp);
-$("#bascorp").change(calculateCorp);
+        $("#deploycorp").html(total.toString());
+
+        if (select == "1") {
+        
+            $("#unitcorp").html("7,345.00");
+    
+            y = 0.1*7565*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feescorp").html(y);
+    
+            totalHyb= (1+0.1)*7565*total;
+            x = totalHyb.formatMoney(2, '.', ',')
+            $("#totalcorp").html(x);
+    
+    
+        } else if (select == "2") {
+    
+            $("#unitcorp").html("12,345.00");
+    
+            y = 0.13*12345*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feescorp").html(y);
+           
+    
+            totalHyb = (1+0.13)*12345*total;
+            x = totalHyb.formatMoney(2, '.', ',');
+            $("#totalcorp").html(x);
+    
+           
+    
+        } else if (select == "3") {
+    
+            $("#unitcorp").html("15,400.00");
+    
+           y = 0.16*15400*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feescorp").html(y);
+    
+            totalHyb= (1+0.16)*15400*total;
+            x = totalHyb.formatMoney(2, '.', ',');
+            $("#totalcorp").html(x);
+        }}
+    });
+});
+
+
+
 
 
 
 //RESIDENTIAL CALCULATIONS //////////////////////////////////////////////
 
 
-
-function calculateRes() {
-
-    var apptsRes = $("#apptsres").val();
-    var floorsRes = $("#floorsres").val();
-    var basesRes = $("#basementsres").val();
+$("#buttonres").click(function(event) {
+    event.preventDefault();
     
-    avgDoors = Math.ceil(apptsRes/floorsRes);
+    var variables = {
+        choiceRes : $("#selectRes").val(),
+        apptsRes : $("#apptsres").val(),
+        basesRes : $("#basementsres").val(), 
+        floorsRes : $("#floorsres").val()
+    };
+    
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(variables),
+        contentType: 'application/json',
+        url: 'https://pacific-forest-47162.herokuapp.com/resserv',						
+        success: function(data) {
+            console.log(data); 
+    
+        total = data.tot;
+        select = data.sel;
 
-    reqShafts = Math.ceil(avgDoors/6);
+        $("#deployres").html(total);
 
-    fl = Number(floorsRes);
-    bs = Number(basesRes);
-    st = fl + bs;
+        if (select == "1") {
+        
+            $("#unitres").html("7,345.00");
+    
+            y = 0.1*7565*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feesres").html(y);
+    
+            totalHyb= (1+0.1)*7565*total;
+            x = totalHyb.formatMoney(2, '.', ',')
+            $("#totalres").html(x);
+    
+    
+        } else if (select == "2") {
+    
+            $("#unitres").html("12,345.00");
+    
+            y = 0.13*12345*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feesres").html(y);
+           
+    
+            totalHyb = (1+0.13)*12345*total;
+            x = totalHyb.formatMoney(2, '.', ',');
+            $("#totalres").html(x);
+    
+           
+    
+        } else if (select == "3") {
+    
+            $("#unitres").html("15,400.00");
+    
+           y = 0.16*15400*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feesres").html(y);
+    
+            totalHyb= (1+0.16)*15400*total;
+            x = totalHyb.formatMoney(2, '.', ',');
+            $("#totalres").html(x);
+        }}
+    });
+});
 
-    columns = 1 + Math.floor(st/20);
 
-     cages = reqShafts*columns;
 
-     //AVOID INCONSISTENCIES IN RESULTS //////////
-     if(isNaN(cages) ||floorsRes ==0) {
-         cages ="";
-     } 
      
      
+     
     
-    var choiceRes = $("#selectRes").val();
-
-    $("#deployres").html(cages.toString());
-
-
-    if (choiceRes == "1") {
-
-        $("#unitres").html("7,565.00");
-
-        x1 = 0.1*7565*cages;
-        x1 = x1.formatMoney(2, '.', ',');
-        $("#feesres").html(x1);
-
-        totalRes= (1+0.1)*7565*cages; 
-        y1 = totalRes.formatMoney(2, '.', ',');
-        $("#totalres").html(y1.toString());
-
-    } else if (choiceRes == "2") {
-
-        $("#unitres").html("12,345.00");
-
-        x1 = 0.13*12345*cages;
-        x1 = x1.formatMoney(2, '.', ',');
-        $("#feesres").html(x1);
-
-        totalRes= (1+0.13)*12345*cages; 
-        y1 = totalRes.formatMoney(2, '.', ',');
-        $("#totalres").html(y1);
-
-    } else if (choiceRes == "3") {
-
-        $("#unitres").html("15,400.00");
-
-        x1 = 0.16*15400*cages;
-        x1 = x1.formatMoney(2, '.', ',');
-        $("#feesres").html(x1);
-
-        totalRes= (1+0.16)*15400*cages; 
-        y1 = totalRes.formatMoney(2, '.', ',');
-        $("#totalres").html(y1);
-
-    }
-
-}
-
-$("#apptsres").change(calculateRes);
-$("#floorsres").change(calculateRes);
-$("#selectRes").change(calculateRes);
+   
 
 //HYBRID CALCULATIONS ////////////////////////////////////
 
-function calculateHyb() {
-    var selectHyb = $("#selecthyb").val();
-    var tenantsHyb = $("#tenantshyb").val();
-    var basementsHyb = $("#bashyb").val();
-    var floorsHyb = $("#floorhyb").val();
+$("#buttonhyb").click(function(event) {
+
+    event.preventDefault();
     
+    var variableHyb = {
 
-    f1 = Number(floorsHyb);
-    b1 = Number(basementsHyb);
+    selectHyb : $("#selecthyb").val(),
+    tenantsHyb : $("#tenantshyb").val(),
+    basementsHyb : $("#bashyb").val(),
+    floorsHyb : $("#floorhyb").val()
 
-    stories1 = f1 + b1;
+    };
+    
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(variableHyb),
+        contentType: 'application/json',
+        url: 'https://pacific-forest-47162.herokuapp.com/hybserv',						
+        success: function(data) {
+            console.log(data); 
+    
+        total = data.tot;
+        select = data.sel;
 
-    totalTenants1 = stories1*tenantsHyb;
+        $("#deployhyb").html(total.toString());
 
-    elevatorsRequired1 = Math.ceil(totalTenants1/1000);
-
-    columnsRequired1 = Math.ceil(stories1/20);
-
-    elevatorPerColumn1 = Math.ceil(elevatorsRequired1/columnsRequired1);
-
-    totalEls1 = elevatorPerColumn1*columnsRequired1;
+        if (select == "1") {
         
-    $("#deployhyb").html(totalEls1);
-
-    if (selectHyb == "1") {
-        
-        $("#unithyb").html("7,345.00");
-
-        y = 0.1*7565*totalEls1;
-        y = y.formatMoney(2, '.', ',');
-        $("#feeshyb").html(y);
-
-        totalHyb= (1+0.1)*7565*totalEls1;
-        x = totalHyb.formatMoney(2, '.', ',')
-        $("#totalhyb").html(x);
-
-
-    } else if (selectHyb == "2") {
-
-        $("#unithyb").html("12,345.00");
-
-        y = 0.13*12345*totalEls1;
-        y = y.formatMoney(2, '.', ',');
-        $("#feeshyb").html(y);
-       
-
-        totalHyb = (1+0.13)*12345*totalEls1;
-        x = totalHyb.formatMoney(2, '.', ',');
-        $("#totalhyb").html(x);
-
-       
-
-    } else if (selectHyb == "3") {
-
-        $("#unithyb").html("15,400.00");
-
-       y = 0.16*15400*totalEls1;
-        y = y.formatMoney(2, '.', ',');
-        $("#feeshyb").html(y);
-
-        totalHyb= (1+0.16)*15400*totalEls1;
-        x = totalHyb.formatMoney(2, '.', ',');
-        $("#totalhyb").html(x);
-
-       
-    }
- }
-
- 
-$("#selecthyb").change(calculateHyb);
-$("#tenantshyb").change(calculateHyb);
-$("#floorhyb").change(calculateHyb);
-$("#bashyb").change(calculateHyb);
-
+            $("#unithyb").html("7,345.00");
+    
+            y = 0.1*7565*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feeshyb").html(y);
+    
+            totalHyb= (1+0.1)*7565*total;
+            x = totalHyb.formatMoney(2, '.', ',')
+            $("#totalhyb").html(x);
+    
+    
+        } else if (select == "2") {
+    
+            $("#unithyb").html("12,345.00");
+    
+            y = 0.13*12345*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feeshyb").html(y);
+           
+    
+            totalHyb = (1+0.13)*12345*total;
+            x = totalHyb.formatMoney(2, '.', ',');
+            $("#totalhyb").html(x);
+    
+           
+    
+        } else if (select == "3") {
+    
+            $("#unithyb").html("15,400.00");
+    
+           y = 0.16*15400*total;
+            y = y.formatMoney(2, '.', ',');
+            $("#feeshyb").html(y);
+    
+            totalHyb= (1+0.16)*15400*total;
+            x = totalHyb.formatMoney(2, '.', ',');
+            $("#totalhyb").html(x);
+        }}
+    });
+});
 
 
 
